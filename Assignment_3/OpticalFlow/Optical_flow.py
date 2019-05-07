7ebf9dc3e180514adf4a45ef7eba8c9139a08eae
Elias <elias.jadon1@gmail.com>
2019-05-07 15:41:42 +0300

    r

--- Assignment_3/OpticalFlow/Optical_flow.py
+++ Assignment_3/OpticalFlow/Optical_flow.py
@@ -1,13 +1,13 @@
 ## Students And Developers: Itay Guy, 305104184 & Elias Jadon, 207755737
 
 import os
-import cv2
-import numpy as np
-import dlib
+#import cv2
+#import numpy as np
+#import dlib
 import pickle
 import time
-import matplotlib.pyplot as plt
-import numpy as np
+#import matplotlib.pyplot as plt
+#import numpy as np
 import operator
 import argparse
 
@@ -16,18 +16,11 @@ testImageDirName = ".//Datasets//Testset"
 
 
 
-def run_optimum(classifier):
-    if classifier == Classifier.NN or classifier == Classifier.SVM:
-        if classifier == Classifier.NN:
-            print("Running NN Classifier..")
-        else:
-            print("Running SVM Classifier..")
-        run(list(), classifier)
-    else:
-        print("A classifier name should be provided.")
+def functionvsd():
+    print("Hi")
 
 
 if __name__ == "__main__":
-    
+    functionvsd()
 
 
