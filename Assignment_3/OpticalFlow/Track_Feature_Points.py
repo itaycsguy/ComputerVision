28c8eea1292724fb088ecf9a2317470fbc17ab7b
itaycsguy <itgu1010@gmail.com>
2019-05-07 21:57:06 +0300

    pulling

--- Assignment_3/OpticalFlow/Track_Feature_Points.py
+++ Assignment_3/OpticalFlow/Track_Feature_Points.py
@@ -23,10 +23,6 @@ selectPoints = True
 numberOfPoints = 7
 
 
-
-
-
-
 def functionvsd():
     # Create a VideoCapture object and read from input file
     # If the input is the camera, pass 0 instead of the video file name
