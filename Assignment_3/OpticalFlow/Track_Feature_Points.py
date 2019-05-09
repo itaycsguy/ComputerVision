541c5b997e18aa509abd4cc9097be1a5959ff1d2
Elias <elias.jadon1@gmail.com>
2019-05-09 21:28:29 +0300

    יעכ

--- Assignment_3/OpticalFlow/Track_Feature_Points.py
+++ Assignment_3/OpticalFlow/Track_Feature_Points.py
@@ -16,12 +16,13 @@ Point_color = (0, 0, 255)
 Point_size = 7
 Line_color = (0, 255, 0)
 Line_size = 3
-Window_Size = 1
+Window_Size = 20
+First_frame = 0
 
 #Input Variables
-inputVideoName = "bugs11.mp4"  #"highway.avi" #""bugs11.mp4"
+inputVideoName = "highway.avi"  #"highway.avi" #""bugs11.mp4"
 selectPoints = True
-numberOfPoints = 5
+numberOfPoints = 10
 
 
 
@@ -75,13 +76,20 @@ def paint_point(points , im):
     return im
 
 
-def paint_velocity(point , velocity , image):
+def paint_velocity(point , velocity , image , Randomcolor = False , index = 0):
+    if Randomcolor:
+        rand = np.mod(np.power(index , 30) , 256)
+        c = (int(rand),int(rand),int(rand))
+
+    else:
+        c = Line_color
+
     for i in range(len(point)):
         p = point[i]
         v = velocity[i]
         from_ = (p[0] , p[1])
         to_ = (int(p[0] + v[0]),int(p[1] + v[1]))
-        cv2.line(image, from_, to_, Line_color, Line_size)
+        cv2.line(image, from_, to_, c, Line_size)
         #print("from = " + str(from_) + " - to = " + to_)
     return image
 
@@ -98,7 +106,7 @@ def GetFrameByIndex(index):
     res, image = cap_video.read()
     if not res:
         print("Error! Could not read image.")
-        exit()
+        #exit()
     cap_video.release()
     return image
 
@@ -107,8 +115,8 @@ def GetFrameByIndex(index):
 def GetPointsFromUser():
     global orig_img, point_img
     global Points
-    orig_img = GetFrameByIndex(0)
-    point_img = GetFrameByIndex(0)
+    orig_img = GetFrameByIndex(First_frame)
+    point_img = GetFrameByIndex(First_frame)
     cv2.namedWindow("Select Points")
     # mouse event listener
     cv2.setMouseCallback("Select Points", mouse_click)
@@ -124,23 +132,36 @@ def GetPointsFromUser():
     return orig_img, point_img, Points
 
 
-def Getderivatives(frame1 , frame2 , centerX , centerY , windowSize):
-    It = (np.average(frame2[centerX - windowSize:centerX + windowSize + 1, centerY - windowSize:centerY + windowSize + 1], axis=2) -
-          np.average(frame1[centerX - windowSize:centerX + windowSize + 1, centerY - windowSize:centerY + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
+def Getderivatives(frame1 , frame2 , centerX1 , centerY1 , centerX2 ,centerY2 , windowSize):
+    It = (np.average(frame2[centerX2 - windowSize:centerX2 + windowSize + 1, centerY2 - windowSize:centerY2 + windowSize + 1], axis=2) -
+          np.average(frame1[centerX1 - windowSize:centerX1 + windowSize + 1, centerY1 - windowSize:centerY1 + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
 
     # It = np.average(fr2[x,y]) - np.average(fr1[x,y])
 
-    Ix = (np.average(frame1[(centerX + 1) - windowSize:(centerX + 1) + windowSize + 1, centerY - windowSize:centerY + windowSize + 1], axis=2) - np.average(
-        frame1[centerX - windowSize:centerX + windowSize + 1, centerY - windowSize:centerY + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
+    Ix = (np.average(frame1[(centerX1 + 1) - windowSize:(centerX1 + 1) + windowSize + 1, centerY1 - windowSize:centerY1 + windowSize + 1], axis=2) - np.average(
+        frame1[centerX1 - windowSize:centerX1 + windowSize + 1, centerY1 - windowSize:centerY1 + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
     # Ix = np.average(fr1[x+1, y]) - np.average(fr1[x, y])
 
-    Iy = (np.average(frame1[centerX - windowSize:centerX + windowSize + 1, (centerY + 1) - windowSize:(centerY + 1) + windowSize + 1], axis=2) - np.average(
-        frame1[centerX - windowSize:centerX + windowSize + 1, centerY - windowSize:centerY + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
+    Iy = (np.average(frame1[centerX1 - windowSize:centerX1 + windowSize + 1, (centerY1 + 1) - windowSize:(centerY1 + 1) + windowSize + 1], axis=2) - np.average(
+        frame1[centerX1 - windowSize:centerX1 + windowSize + 1, centerY1 - windowSize:centerY1 + windowSize + 1], axis=2)).reshape(np.power(2 * windowSize + 1, 2), 1)
     # Iy = np.average(fr1[x, y+1]) - np.average(fr1[x, y])
     return It,Ix,Iy
 
 def Lucas_Kanade_system(Ix,Iy,It):
     A = np.concatenate((Ix, Iy), axis=1)
+    b = -1 * It
+    At = np.transpose(A)
+    AtA = np.dot(At, A)
+    Atb = np.dot(At, b)
+    if (np.linalg.det(AtA) == 0):
+        return [0.0,0.0]
+    x = np.linalg.solve(AtA, Atb)
+    return np.round(np.transpose(x)[0])
+
+def Affine_Lucas_Kanade_system(Ix,Iy,It):
+    A = np.concatenate((Ix, Iy), axis=1)
+
+
     b = -1 * It
     At = np.transpose(A)
     AtA = np.dot(At, A)
@@ -159,8 +180,7 @@ if __name__ == "__main__":
         #We need to choose a number of them (like the parameter we have)
         print("TODO: Choose points from HOG")
 
-
-
+    height, width, channels = orig_img.shape
 
     cap = cv2.VideoCapture(".//Datasets//" + inputVideoName)
     # Check if camera opened successfully
@@ -168,8 +188,13 @@ if __name__ == "__main__":
         print("Error opening video stream or file")
         exit()
 
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(".//Results//Video" + inputVideoName, fourcc, 20.0, (width,height))
+    LastFrame = GetFrameByIndex(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1)
 
-    for indexFrame in range(0,int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1):
+
+    for indexFrame in range(First_frame,int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1):
         print(">> " + str(indexFrame))
         fr1 = GetFrameByIndex(indexFrame).astype(float)
         fr2 = GetFrameByIndex(indexFrame+1).astype(float)
@@ -186,34 +211,50 @@ if __name__ == "__main__":
             print("x = " + str(x))
             print("y = " + str(y))
 
-            It, Ix, Iy = Getderivatives(fr1 , fr2 , x , y , Window_Size)
-            solution = Lucas_Kanade_system(Ix,Iy,It)
-            print("solution = " + str(solution))
 
+            UpdateX = x
+            UpdateY = y
+            round = 0
+            while round < 9:
+                It, Ix, Iy = Getderivatives(fr1 , fr2 , x , y , UpdateX , UpdateY, Window_Size)
+                solution = Lucas_Kanade_system(Ix,Iy,It)
+                print("solution = " + str(solution))
 
+                UpdateX = int(UpdateX + solution[0])
+                UpdateY = int(UpdateY + solution[1])
 
+                if (solution[0] == 0) and (solution[1] == 0):
+                    break
+                if (UpdateX >= (height-Window_Size-1)):
+                    UpdateX = height - Window_Size-2
+                    break
+                if (UpdateY >= (width-Window_Size-1)):
+                    UpdateY = width -Window_Size- 2
+                    break
+                if (UpdateX <= Window_Size):
+                    UpdateX = 1+Window_Size
+                    break
+                if (UpdateY <= Window_Size):
+                    UpdateY = 1+Window_Size
+                    break
+                round = round+1
 
-            height, width, channels = fr1.shape
-
-            UpdateX = int(x + solution[0])
-            UpdateY = int(y + solution[1])
-
-            if (UpdateX >= (height-Window_Size-1)):
-                UpdateX = height - Window_Size-2
-            if (UpdateY >= (width-Window_Size-1)):
-                UpdateY = width -Window_Size- 2
-            if (UpdateX <= Window_Size):
-                UpdateX = 1+Window_Size
-            if (UpdateY <= Window_Size):
-                UpdateY = 1+Window_Size
-
+            LastFrame = paint_velocity([[x,y]], [[UpdateX - x , UpdateY - y]], LastFrame , True , i)
             Points[i] = (UpdateY , UpdateX)
-            velocity.append((solution[0] , solution[1]))
+            velocity.append((UpdateX - x , UpdateY - y))
             i=i+1
 
         print(Points)
         draw_im = paint_velocity(Points, velocity, draw_im)
         cv2.imshow("added points", draw_im)
+        out.write(draw_im)
         cv2.waitKey(1)
+
+
+    cv2.imwrite(".//Results//Velocity" + inputVideoName+".jpg", LastFrame)
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
