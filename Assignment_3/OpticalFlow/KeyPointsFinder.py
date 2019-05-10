4726b5d4b1b67cf2c86280bba23369bf362d7d7a
itaycsguy <itgu1010@gmail.com>
2019-05-10 10:20:00 +0300

    made hog algorithm better
    TODO: pick stronger locations from the maximum block who has been picked

--- Assignment_3/OpticalFlow/KeyPointsFinder.py
+++ Assignment_3/OpticalFlow/KeyPointsFinder.py
@@ -5,9 +5,11 @@ import numpy as np
 class KeyPointsFinder:
 
     """
-        [1] cell_size - size of block to divide the image, default: 8 -> (8, 8)
-        [2] bin_n     - number of bins for the histogram at each cell, default: 16
-        [3] win_size  - size the image (expecting to 2^x type of size), default: (128, 64)
+    KeyPointsFinder(image_path[, cell_size=8[, bin_n=9[, win_size=(128, 64)]]]) -> HOG descriptor
+    .   @brief Computes the hog descriptor per an image.
+    .   @param cell_size Size of the divided image.
+    .   @param bin_n Size of the histogram per block.
+    .   @param win_size Size of the scaled image (cols, rows).
     """
     def __init__(self, image_path, cell_size=8, bin_n=9, win_size=(128, 64)):
         # These are parameters which required from the program to get initially
@@ -15,27 +17,32 @@ class KeyPointsFinder:
         self._bin_n = bin_n             # Number of bins
         self._win_size = win_size
         self._real_image = cv2.imread(image_path)
-        self._resized_image = cv2.resize(self._real_image, self._win_size, interpolation=cv2.INTER_CUBIC)
-        self._feature_vectors = None
+        self._scaled_image = cv2.resize(self._real_image, self._win_size, interpolation=cv2.INTER_CUBIC)
+        self._hog_descriptor = None
+        self._grad = None
+        self._angleOfs = None
 
 
     """
-        Return the original image
+    get_real_image() -> original image
+    .   @brief Getting the original input image.
     """
     def get_real_image(self):
         return self._real_image
 
 
     """
-        Return the resized image
+    get_scaled_image() -> scaled image
+    .   @brief Getting the scaled input image.
     """
-    def get_resized_image(self):
-        return self._resized_image
+    def get_scaled_image(self):
+        return self._scaled_image
 
 
     """
-        Return a reshaped structure of the HOG descriptor
-        [1] raw_hist - the raw data that HOG finds
+    __reshape(raw_hist) -> reshaped hog descriptor
+    .   @brief Reshaping the hog descriptor.
+    .   @param raw_hist A raw data normalized histogram.
     """
     def __reshape(self, raw_hist):
         hists = list()
@@ -46,8 +53,9 @@ class KeyPointsFinder:
 
 
     """
-        Reducing one dimensionality
-        [1] multi_feature_vectors - list[list[list[number]]] type of structure to list[list[number]]
+    __reduce_dimension(multi_feature_vectors) -> reduced dimension array
+    .   @brief Reduces a dimension.
+    .   @param multi_feature_vectors A multi-dimensional array of features.
     """
     def __reduce_dimension(self, multi_feature_vectors):
         reduced = []
@@ -59,8 +67,9 @@ class KeyPointsFinder:
 
 
     """
-        HOG descriptor 
-        [1] image - an openCV image
+    get_native_hog(image) -> HOG descriptor
+    .   @brief Computes the hog descriptor per an image.
+    .   @param image An image.
     """
     def get_native_hog(self, image):
         winSize = (image.shape[1], image.shape[0])
@@ -80,59 +89,84 @@ class KeyPointsFinder:
                                          gammaCorrection, nlevels, signedGradient)
 
         # compute(img[, winStride[, padding[, locations]]]) -> descriptors
-        winStride = (self._cell_size, self._cell_size)
+        winStride = winSize
         padding = (self._cell_size, self._cell_size)
         # correlated with n-bins number -> not working at all:
         locations = []
         hist = hog_instance.compute(image, winStride, padding, locations)
-        # return => |hist| = 1152 * 9 = |descriptor| * 9? => 128 blocks * 9 bins * 9?
-        return self.__reshape(hist)
+        grad = np.zeros(winStride, dtype=np.float32)
+        angleOfs = np.zeros(winStride, dtype=np.float32)
+        grad, angleOfs = hog_instance.computeGradient(image, grad, angleOfs)
+        return self.__reshape(hist), self.__reshape(grad), self.__reshape(angleOfs)
 
 
     """
-        Generating all possible key-points at the current executed image
+    generate_key_points() -> None
+    .   @brief Computes all possible key-points at the current image.
     """
     def generate_key_points(self):
-        self._feature_vectors = np.asarray(self.__reduce_dimension(self.get_native_hog(self._resized_image)), dtype=np.float32)
+        self._hog_descriptor, self._grad, self._angleOfs = self.get_native_hog(self._scaled_image)
+        self._hog_descriptor = np.asarray(self.__reduce_dimension(self._hog_descriptor), dtype=np.float32)
 
 
     """
-        Get the n strongest key points from all image that were found
-        [1] img - an image
-        [2] n   - number of key points to find
+    get_strongest(img[, n]]) -> key_points indices
+    .   @brief Computes the strongest key-points from HOG descriptor.
+    .   @param img An image.
+    .   @param n Number of key points to find.
     """
-    def get_strongest(self, img, n):
-        data_len = len(self._feature_vectors)
-        # Descriptor vector per block
-        data_len = data_len / (self._bin_n * self._cell_size**2)
-        blocks_idx = np.flip(np.argsort(np.asarray(self._feature_vectors)))[:n]
-        blocks_loc = self._cell_size**2 * np.asarray(blocks_idx / data_len, dtype=np.uint32)
-        rows = np.asarray(blocks_loc / img.shape[0], dtype=np.uint32)
-        cols = np.asarray(blocks_loc / img.shape[1], dtype=np.uint32)
+    def get_strongest(self, n):
+        row_blocks = self._scaled_image.shape[1] / self._cell_size
+        col_blocks = self._scaled_image.shape[0] / self._cell_size
+        descriptor_size = len(self._hog_descriptor)
+        descriptor_size_per_block = int(descriptor_size / (row_blocks * col_blocks))
 
-        key_points_idx = list()
+        descriptors_strength = list()
+        for i in range(0, descriptor_size, descriptor_size_per_block):
+            descriptors_strength.append(np.sum(self._hog_descriptor[i:(i + descriptor_size_per_block)]))
+
+        # sorted by the maximum
+        sorted_blocks_idx = np.flip(np.argsort(descriptors_strength))
+        int_blocks = np.asarray(sorted_blocks_idx / col_blocks, dtype=np.uint32)
+        rows = int_blocks * self._cell_size
+        rows = rows[:n]
+        cols = np.asarray(np.asarray((sorted_blocks_idx / col_blocks) - int_blocks, dtype=np.float32) * col_blocks * self._cell_size, dtype=np.uint32)
+        cols = cols[:n]
+
+        # TODO:
+        """
+        need to pick up stronger locations 
+        at the maximum block if it exists 
+        until the first less then the next maximum block will arrived
+        """
+
+        locations = list()
         for x, y in zip(rows, cols):
-            key_points_idx.append((x, y))
+            point = (x, y)
+            locations.append(point)
 
-        return key_points_idx
+        return locations
 
 
     """
-        Finding n key points at the image
+    get_key_points(n) -> key_points
+    .   @brief Computes n key-points as a wrapper function.
+    .   @param n Number of key points to find.
     """
     def get_key_points(self, n):
         self.generate_key_points()
-        return finder.get_strongest(self._resized_image, n)
+        return finder.get_strongest(n)
 
 
     """
-        Plot found key points on the image
-        [1] key_points - an array of key points
+    plot_key_points(key_points) -> None
+    .   @brief Plotting the key-points on the image.
+    .   @param key_points Key-points that have been found.
     """
     def plot_key_points(self, key_points):
         for point in key_points:
-            cv2.circle(self._resized_image, point, 5, (0, 255, 0), -1)
-        cv2.imshow('HOG Image Strongest Key Points', self._resized_image)
+            cv2.circle(self._scaled_image, point, 5, (0, 255, 0), -1)
+        cv2.imshow('HOG Image Strongest Key Points', self._scaled_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -142,6 +176,6 @@ if __name__ == "__main__":
     sys_path = "D:\\PycharmProjects\\ComputerVision\\Assignment_3\\OpticalFlow\\Datasets\\"
     image = sys_path + "image001.jpg"
     finder = KeyPointsFinder(image)
-    key_points = finder.get_key_points(50)
+    key_points = finder.get_key_points(5)
     finder.plot_key_points(key_points)
 
